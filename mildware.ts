4168a3ca445474aa88d8d1db3115812bb0ef2bc6
Tanushree <tanushreedhana@gmail.com>
2026-05-11 12:38:25 +0530

    Protected dashboard routes

---
+++ mildware.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(
+  request: NextRequest
+) {
+
+  const token =
+    request.cookies.get(
+      "sb-access-token"
+    );
+
+  if (
+    !token &&
+    request.nextUrl.pathname.startsWith(
+      "/dashboard"
+    )
+  ) {
+
+    return NextResponse.redirect(
+      new URL(
+        "/auth",
+        request.url
+      )
+    );
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    "/dashboard/:path*",
+  ],
+};
